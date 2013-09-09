d77638b3de56360531c7c4fc6a94a92c89900c58
Francois Wauquier <wokier@gmail.com>
2013-09-09 12:59:56 +0200

    clojars deploy

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject lein-bower "0.1.0"
+(defproject org.clojars.wokier/lein-bower "0.1.0"
   :description "Clojure Leiningen Plugin for Bower web lib dependency management "
-  :url "http://example.com/FIXME"
+  :url "https://github.com/wokier/lein-bower"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :eval-in-leiningen true)
